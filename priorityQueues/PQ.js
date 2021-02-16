1debff93f1e365c8232a9e8d724ed89ed73c5c94
ShubhamBhardwaj <shubhambhardwajfdb@gmail.com>
2021-02-16 16:24:03 +0530

    insert queue

--- priorityQueues/PQ.js
+++ priorityQueues/PQ.js
@@ -9,6 +9,30 @@ class priorityQueue {
   constructor() {
     this.values = [];
   }
+  insert = (val, priority) => {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    let idx = this.values.length - 1;
+    const nodeValue = this.values[idx].priority;
+    while (true) {
+      let temp = this.values[Math.floor(idx / 2)].priority;
+      if (temp >= nodeValue) {
+        break;
+      }
+
+      this.values[Math.floor(idx / 2)].priority = nodeValue;
+      this.values[idx].priority = temp;
+
+      idx = Math.floor(idx / 2);
+    }
+    return this.values;
+  };
 }
 
-const pq = new priorityQueue([]);
+const pq = new priorityQueue();
+console.log(pq.insert("play", 41));
+console.log(pq.insert("play", 39));
+console.log(pq.insert("play", 33));
+console.log(pq.insert("play", 18));
+console.log(pq.insert("play", 27));
+console.log(pq.insert("play", 12));
