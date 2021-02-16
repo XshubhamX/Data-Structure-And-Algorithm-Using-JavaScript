ebed8b338459b84657749884ce37dfcceab23c62
ShubhamBhardwaj <shubhambhardwajfdb@gmail.com>
2021-02-16 16:16:16 +0530

    pq initial setup

---
+++ priorityQueues/PQ.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
+class priorityQueue {
+  constructor() {
+    this.values = [];
+  }
+}
+
+const pq = new priorityQueue([]);
