401e469d2247539134fdc477bbf03e93ca505050
ShubhamBhardwaj <shubhambhardwajfdb@gmail.com>
2021-02-16 15:56:21 +0530

    extractMax

--- Heap/Heap.js
+++ Heap/Heap.js
@@ -20,8 +20,46 @@ class MaxHeap {
     }
     return this.values;
   };
+
+  swap = (i, j) => {
+    let temp = this.values[i];
+    this.values[i] = this.values[j];
+    this.values[j] = temp;
+  };
+
+  removeMax = () => {
+    let idx = 0,
+      i = 0;
+    const popped = this.values.pop();
+    this.values[0] = popped;
+    let lChild, rChild;
+    let length = this.values.length;
+    let swap = null;
+    while (true) {
+      swap = null;
+      i = 2 * idx;
+      lChild = this.values[i + 1];
+      rChild = this.values[i + 2];
+
+      if (i + 1 < length) {
+        if (lChild > rChild) {
+          this.swap(idx, i + 1);
+          swap = 1;
+          idx = i + 1;
+        } else if (rChild > lChild) {
+          this.swap(idx, i + 2);
+          swap = 1;
+          idx = i + 2;
+        }
+      }
+      if (!swap) {
+        break;
+      }
+    }
+    return this.values;
+  };
 }
 
-const maxheap = new MaxHeap([41, 39, 33, 18, 27, 12, 55]);
+const maxheap = new MaxHeap([41, 39, 33, 18, 27, 12]);
 
-console.log(maxheap.insert(55));
+console.log(maxheap.removeMax());
