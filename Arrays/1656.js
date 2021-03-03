1ef21bef704793949c7efaec9d35e612e3d83d49
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-03-03 16:44:46 +0530

    1656

--- Arrays/1656.js
+++ Arrays/1656.js
@@ -1,1 +1,61 @@
+class OrderedStream {
+  constructor(n) {
+    this.n = n;
+    this.stream = new Array(n);
+    this.helper = new Array(n).fill(0);
+  }
 
+  insert = ([idKey, value]) => {
+    if (idKey >= this.n) {
+      return null;
+    }
+    let i = 0,
+      j = 0,
+      returnList = [];
+    this.stream[idKey] = [idKey, value];
+    if (idKey === 0) {
+      while (this.helper[i] === 0 && this.stream[i] !== undefined) {
+        this.helper[i] = 1;
+        returnList.push(this.stream[i]);
+        i++;
+      }
+      return returnList;
+    } else if (idKey === this.n - 1) {
+      i = idKey;
+      while (this.helper[i] === 0 && this.stream[i] !== undefined) {
+        i--;
+        this.helper[i] = 1;
+      }
+      if (i === idKey) {
+        return null;
+      }
+      for (i; i <= idKey; i++) {
+        returnList.push(this.stream[i]);
+        return returnList;
+      }
+    } else {
+      i = idKey;
+      while (this.helper[i] === 0 && this.stream[i] !== undefined) {
+        i--;
+        this.helper[i] = 1;
+      }
+      j = idKey;
+      while (this.helper[i] === 0 && this.stream[i] !== undefined) {
+        helper[i] = 1;
+        i++;
+      }
+      if ((i = j)) {
+        return null;
+      }
+      for (i; i <= j; i++) {
+        returnList.push(this.this.stream[i]);
+      }
+      return returnList;
+    }
+  };
+}
+
+const os = new OrderedStream(5);
+
+console.log(os.insert([2, "ada"]));
+console.log(os.insert([1, "ada"]));
